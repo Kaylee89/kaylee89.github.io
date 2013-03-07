659d1839f1d5d1befb86c974f4a63badb97bc123
Kaylee89 <jiehuei@gmail.com>
2013-03-07 21:15:30 +1300

    slider plugin working, added buttons

--- js/main.js
+++ js/main.js
@@ -5,4 +5,30 @@ $(document).ready(function() {
 			enableKeys: "true"			
 	});
 	
+
+/*	Bakery page
+	========================================= */
+	$('#bakeryPort').contentSlide({
+			start : 1
+		,	speed : 2000
+	})
+
+	$('a[href="#nextSlide"]').click(function(){
+		$(this).closest('.wideViewport').contentSlide('to', 2);
+		// You can use:
+		// $viewport.contentSlide('next')
+		// $viewport.contentSlide('prev')
+		// $viewport.contentSlide('to', {index})
+		// Index starts @ 0
+	});
+
+	$('a[href="#prevSlide"]').click(function(){
+		$(this).closest('.wideViewport').contentSlide('to', 0);
+	});
+
+	$('a[href="#prevSlide"]').click(function(){
+		$(this).closest('.wideViewport').contentSlide('to', 1);
+	});
+	
+
 });
